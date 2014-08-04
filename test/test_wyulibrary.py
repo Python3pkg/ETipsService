f8efc4c92ce4b5e847f7b98bc9bc46c52b856357
Jayin Ton <tonjayin@gmail.com>
2014-08-04 14:03:00 +0800

    add simple test

--- test/test_wyulibrary.py
+++ test/test_wyulibrary.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from service.wyulibrary import WyuLibrary
+
+if __name__ == '__main__':
+    lib = WyuLibrary()
+    print lib.search_book("java")
--- test/test_wyunews.py
+++ test/test_wyunews.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from service.wyunews import WyuNews
+
+if __name__ == '__main__':
+    wyunews = WyuNews()
+    print (wyunews.get_wyu_news(1))
