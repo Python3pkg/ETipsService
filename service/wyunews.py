e52a05352224ed56a6195e39d91a5d56eed1e5b5
Jayin Ton <tonjayin@gmail.com>
2014-08-21 21:39:29 +0800

    more deatils return

--- service/wyunews.py
+++ service/wyunews.py
@@ -10,7 +10,7 @@ class WyuNews(object):
         pass
 
     @staticmethod
-    def _get_tag_a(tag):
+    def __get_tag_a(tag):
         if tag.has_attr('href') and tag.has_attr('target'):
             if 'http://' in tag.attrs['href']:
                 return False
@@ -18,13 +18,30 @@ class WyuNews(object):
         return False
 
     @staticmethod
-    def _wyu_news( page):
+    def __get_tag_td(tag):
+        if tag.has_attr('height') and tag['height'] == '18' and tag.get_text() != '':
+            return True
+        return False
+
+    @staticmethod
+    def __wyu_news(page):
         url = 'http://www.wyu.cn/news/default.asp'
         params = {'page': page}
         r = requests.get(url, params=params)
         encoding = _.get_charset(r.content)
         return r.content.decode(encoding)
 
+    @staticmethod
+    def __get_news_type(tag):
+        return tag.get_text().split(u"  ")[0].lstrip(u'【').rstrip(u'】')
+
+    @staticmethod
+    def __get_news_from(tag):
+        return tag.get_text().split(u"  ")[1].lstrip(u' ').rstrip(u' ')
+
+    @staticmethod
+    def __get_news_posttime(tag):
+        return tag.get_text().split(u"  ")[2].strip(u' ')
 
     def get_wyu_news(self, page):
         response = {
@@ -32,14 +49,21 @@ class WyuNews(object):
         }
         if page <= 0:
             return response
-        res = WyuNews._wyu_news(page)
+        res = WyuNews.__wyu_news(page)
         soup = BeautifulSoup(res, from_encoding='utf-8')
-        tag_a = soup.find_all(self._get_tag_a)
+        tag_a = soup.find_all(self.__get_tag_a)
+        tag_td = soup.find_all(self.__get_tag_td)
         result = []
-        for item in tag_a:
+
+        for index, item in enumerate(tag_a):
             result.append({
                 'url': ''.join(('http://www.wyu.cn/news/', item.attrs['href']))
                 , 'title': item.string
+                , 'type': self.__get_news_type(tag_td[index])
+                , 'from': self.__get_news_from(tag_td[index])
+                , 'posttime': self.__get_news_posttime(tag_td[index])
             })
         response['result'] = result
         return _.to_json_string(response)
+
+
